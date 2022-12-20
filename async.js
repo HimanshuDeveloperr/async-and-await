aa283351fc34854af6713c8df3dc99c38fc63a96
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2022-12-21 01:27:27 +0530

    promise colddrinks

--- async.js
+++ async.js
@@ -2,52 +2,37 @@ console.log("person1:shows tickets")
 console.log("person2:shows tickets")
 
 
-const preMovie= async ()=>{
+const myWifeBringingTickets= new Promise((resolve,reject)=>{
+    setTimeout(() => {
 
-    const myWifeBringingTickets= new Promise((resolve,reject)=>{
-        setTimeout(() => {
-    
-            resolve("ticket")
-            
-        }, 3000);
-    })
-
-    const getpopcorn=new Promise((resolve,reject)=>{
-        resolve(`popcorn`)
-        })
-    
-        const getbutter= new Promise((resolve,reject)=>{
-                resolve(`butter`)
-            })
+        resolve("ticket")
         
-            const getColdDrinks=new Promise((resolve,reject)=>{
-                resolve(`colddrinks`)
-            })
+    }, 3000);
+})
 
-    let ticket= await myWifeBringingTickets;
-    console.log(`wife:i got the ${ticket}`)
-
+const popcorn=myWifeBringingTickets.then((t)=>{
     console.log('husband:we should go in')
     console.log('wife:im hungry')
 
-    let popcorn=await getpopcorn
-    console.log(`husband:i will buy ${popcorn}`)
-    console.log(`wife:i need butter in my ${popcorn}`)
+    return new Promise((resolve,reject)=>{
+    resolve(` ${t}`)
+    })
+})
 
-    let butter=await getbutter
-    console.log(`ok,will remember that ${butter} now lets go with the ${ticket}`)
+const getbutter=popcorn.then((t)=>{
+    console.log('wife:i need butter in my popcorn')
+    return new Promise((resolve,reject)=>{
+        resolve(`ok,will remember that now lets go with the ${t}`)
+    })
+})
 
-    let colddrinks= await getColdDrinks;
-    console.log(`wife: when you are at it could you also bring me some ${colddrinks}`)
-    console.log(`husband: ok , now shall we `)
+const getcoldrink=getbutter.then((t)=>{console.log(`wife:please bring some colddrinks also`)
+                 return new Promise((resolve,reject)=>{
+                    resolve(`${t}`)
+                 })
+})
 
-    return ticket
-
-
-}
-
-
-preMovie().then((m)=>{console.log(`person3:shows ${m}`)})
+getcoldrink.then((t)=>{console.log(`${t}, finally, person3:shows tickets`)})
 
 console.log("person4:shows tickets")
 console.log("person5:shows tickets")
