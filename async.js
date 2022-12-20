0a8441851fe2b74c41a93bdc10f8146b4ddbc20d
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2022-12-21 00:37:33 +0530

    promise method

---
+++ async.js
@@ -0,0 +1,31 @@
+console.log("person1:shows tickets")
+console.log("person2:shows tickets")
+
+const myWifeBringingTickets= new Promise((resolve,reject)=>{
+    setTimeout(() => {
+
+        resolve("ticket")
+        
+    }, 3000);
+})
+
+const popcorn=myWifeBringingTickets.then((t)=>{
+    console.log('husband:we should go in')
+    console.log('wife:im hungry')
+
+    return new Promise((resolve,reject)=>{
+    resolve(`${t}`)
+    })
+})
+
+const getbutter=popcorn.then((t)=>{
+    console.log('wife:i need butter in my popcorn')
+    return new Promise((resolve,reject)=>{
+        resolve(`ok,will remember that now lets go with the ${t}`)
+    })
+})
+
+getbutter.then((t)=>{console.log(t)})
+
+console.log("person4:shows tickets")
+console.log("person5:shows tickets")
